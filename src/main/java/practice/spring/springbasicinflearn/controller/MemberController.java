d51aa7816ec240cd2264d4229c7f206d2e07a63d
dongsub_youn <es001311@korea.ac.kr>
2022-01-13 18:00:27 +0900

    feat(controller): add MemberController class

---
+++ src/main/java/practice/spring/springbasicinflearn/controller/MemberController.java
@@ -0,0 +1,15 @@
+package practice.spring.springbasicinflearn.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import practice.spring.springbasicinflearn.service.MemberService;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
