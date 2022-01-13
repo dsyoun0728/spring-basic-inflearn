68e108cb5c7735f2b8be1ba4c02a621ae9480abc
dongsub_youn <es001311@korea.ac.kr>
2022-01-13 13:53:28 +0900

    feat(repository): add MemberService interface and implement class

---
+++ src/main/java/practice/spring/springbasicinflearn/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package practice.spring.springbasicinflearn.repository;
+
+import practice.spring.springbasicinflearn.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
+
