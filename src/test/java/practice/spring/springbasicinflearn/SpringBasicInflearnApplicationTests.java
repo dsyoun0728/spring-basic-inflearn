210ee3f0f428c6414e77d6b6aae43261e0307488
dongsub_youn <es001311@korea.ac.kr>
2022-01-13 10:35:01 +0900

    chore: init spring boot project

---
+++ src/test/java/practice/spring/springbasicinflearn/SpringBasicInflearnApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.spring.springbasicinflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicInflearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
