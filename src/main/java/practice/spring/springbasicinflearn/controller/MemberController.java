07caf13eaeccf36a57a5111df415b15666c52c98
dongsub_youn <es001311@korea.ac.kr>
2022-01-18 13:38:58 +0900

    feat: add registering member components

--- src/main/java/practice/spring/springbasicinflearn/controller/MemberController.java
+++ src/main/java/practice/spring/springbasicinflearn/controller/MemberController.java
@@ -2,6 +2,9 @@ package practice.spring.springbasicinflearn.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import practice.spring.springbasicinflearn.domain.Member;
 import practice.spring.springbasicinflearn.service.MemberService;
 
 @Controller
@@ -11,5 +14,20 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
