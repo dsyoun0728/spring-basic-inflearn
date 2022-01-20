6a871d75e03429fa3454098bfca411927d32999e
dongsub_youn <es001311@korea.ac.kr>
2022-01-20 16:14:51 +0900

    fix(bean): use JPA-based Impl Class

--- src/main/java/practice/spring/springbasicinflearn/SpringConfig.java
+++ src/main/java/practice/spring/springbasicinflearn/SpringConfig.java
@@ -3,22 +3,29 @@ package practice.spring.springbasicinflearn;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import practice.spring.springbasicinflearn.repository.JdbcMemberRepository;
-import practice.spring.springbasicinflearn.repository.JdbcTemplateMemberRepository;
-import practice.spring.springbasicinflearn.repository.MemberRepository;
-import practice.spring.springbasicinflearn.repository.MemoryMemberRepository;
+import practice.spring.springbasicinflearn.repository.*;
 import practice.spring.springbasicinflearn.service.MemberService;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    // 순수 JDBC와 JdbcTemplate 사용 시 필요한 부분
+//    private DataSource dataSource;
+//
+//    @Autowired
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+
+    // JPA 사용 시 필요한 부분
+    private EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -36,7 +43,10 @@ public class SpringConfig {
 //        return new JdbcMemberRepository(dataSource);
 
         // JdbcTemplate 기반의 MemberRepository 구현 클래스 사용
-        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+
+        // JPA 기반의 MemberRepository 구현 클래스 사용
+        return new JpaMemberRepository(em);
     }
 
 }
